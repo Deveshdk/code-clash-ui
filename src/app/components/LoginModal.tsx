608f682111665acd1d94630c2c47e528ea4326fc
Devesh Kumar <78483496+Deveshdk@users.noreply.github.com>
2025-09-27 23:39:31 +0530

    added login button and dialog (#3)

---
+++ src/app/components/LoginModal.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Button,
+  Typography
+} from '@mui/material';
+import { ILoginModalProps } from '../interfaces';
+
+export const LoginModal : React.FC<ILoginModalProps> = ({isModalOpen, onClose}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = () => {
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
+    return (
+        <Dialog open={isModalOpen} onClose={onClose}>
+        <DialogTitle>
+          <Typography variant="h6">Log In</Typography>
+        </DialogTitle>
+
+        <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 1 }}>
+          <TextField
+            label="Username"
+            variant="outlined"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            variant="outlined"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button variant="contained" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+};
--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -1,13 +1,22 @@
-import React from "react";
-import { LoginGrid, StyledButton } from "../styles/LoginStyle";
+import React, { useState } from "react";
+import { LoginGrid, LoginModalGrid, StyledButton } from "../styles/LoginStyle";
 import { LOGIN_BUTTON } from "../constants/CodeClashLoginConstants";
+import LoginIcon from "@mui/icons-material/Login";
+import { LoginModal } from "../components";
+
 
 
 export const Login: React.FC = () => {
+    const [isLogInModalOpen, setIsLogInModalOpen] = useState<boolean>(false);
+
+    const handleOnClose = () => {
+        setIsLogInModalOpen(false);
+    }
 
     return (
         <LoginGrid>
-            <StyledButton onClick={() => {}}>{LOGIN_BUTTON}</StyledButton>
+            <StyledButton onClick={()=> setIsLogInModalOpen(true)}><LoginIcon sx={{ mr: 1 }}/>{LOGIN_BUTTON}</StyledButton>
+            <LoginModal isModalOpen={isLogInModalOpen} onClose={handleOnClose}/>
         </LoginGrid>
     );
 };
